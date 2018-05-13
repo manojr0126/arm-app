7be7e9941fbc3f9a90cf3b46aafee81592ab8eb3
Manoj Radhakrishnan <manojr0126@gmail.com>
2018-05-13 19:14:54 -0400

    Added project files

---
+++ src/app/dashboard/app.dashboardComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.dashboardComponent.html',
+  styleUrls: ['./app.dashboardComponent.css']
+})
+export class AppComponent {
+  title = 'Anthem Resource Management';
+}
