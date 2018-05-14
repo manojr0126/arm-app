317a9278b742ad29b06076c89d7f70927ca1c285
Manoj Radhakrishnan <manojr0126@gmail.com>
2018-05-13 22:21:07 -0400

    Modified files

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+var express = require('express');
+var path = require('path');
+var app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || '5000';
+
+app.set('port', port);
+app.use(express.static(__dirname + '/dist/arm-app'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+    res.set('Content-Type', 'text/html')
+        .sendFile(path.join(__dirname + '/dist/arm-app/index.html'))
+});
 
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost" + app.get('port'))
+});
