2966d2093e958312568fdf75216f86763b2195dc
Manoj Radhakrishnan <manojr0126@gmail.com>
2018-05-13 23:43:45 -0400

    Modified files

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-const port = process.env.PORT || '8080';
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
+
+/*const port = process.env.PORT || '8080';
 
 app.set('port', port);
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
     res.set('Content-Type', 'text/html')
-        .sendFile(path.join(__dirname + '/index.html'))
+        .sendFile(path.join(__dirname + '/dist/index.html'))
 });
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost" + app.get('port'))
-});
+});*/
