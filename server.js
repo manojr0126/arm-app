8e67ca6516ceade3aa6a1116b080dd096c591a4d
Manoj Radhakrishnan <manojr0126@gmail.com>
2018-05-13 23:24:25 -0400

    Modified files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || '8080';
 
 app.set('port', port);
 app.use(express.static(__dirname + '/dist/arm-app'));
