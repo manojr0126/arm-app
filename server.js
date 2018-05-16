10abd978019c57f4e6e1706041933635c38678de
Manoj Radhakrishnan <manojr0126@gmail.com>
2018-05-16 01:32:16 -0400

    Modified port

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var express = require('express');
 var app = express();
 var http = require('http');
 
-var port = process.env.PORT || '80';
+var port = process.env.PORT || '3000';
 
 app.set('port', port);
 
