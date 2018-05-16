e4fba2b494a1d0fa25825526eed76f45662eef9c
Manoj Radhakrishnan <manojr0126@gmail.com>
2018-05-16 01:24:38 -0400

    Modified port

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost - " + app.get('port'))
 });*/
 
-var app = require('express');
+var express = require('express');
+var app = express();
 var http = require('http');
 
 var port = process.env.PORT || '80';
