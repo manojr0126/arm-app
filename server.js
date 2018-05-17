acc40f488d84a19cbed701d5fbd9b931651e6782
Manoj Radhakrishnan <manoj.radhakrishnan2@anthem.com>
2018-05-16 21:48:26 -0400

    Modified server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ server.listen(port, function () {
 
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 express()
   .use(express.static(path.join(__dirname, 'src/assets')))
